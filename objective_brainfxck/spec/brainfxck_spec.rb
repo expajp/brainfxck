4104d19f381bbeaf3b8283920bd1ce67b3ad6591
Shu OGAWARA <expajp@gmail.com>
2019-06-14 23:52:51 +0900

    implement test for fizzbuzz

--- objective_brainfxck/spec/brainfxck_spec.rb
+++ objective_brainfxck/spec/brainfxck_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BrainFxck do
   describe 'run sample codes' do
     before do 
       $stdout = StringIO.new
-      File.open('../sample_codes/hello.bf') do |file|
+      File.open(source_path) do |file|
         BrainFxck.new(file.read).run
       end
     end
@@ -12,8 +12,17 @@ RSpec.describe BrainFxck do
     describe 'display hello_world' do
       let(:source_path){ '../sample_codes/hello.bf' }
 
-      it 'display hello_world' do
+      it 'return Hello World!' do
         expect($stdout.string).to eq "Hello World!\n\n"
+      end
+    end
+
+    describe 'display fizzbuzz' do
+      let(:source_path){ '../sample_codes/fizzbuzz.bf' }
+
+      it 'display fizzbuzz' do
+        answer = (1..100).to_a.map{ |n| n%3 == 0 ? (n%5 == 0 ? 'FizzBuzz' : 'Fizz') : (n%5 == 0 ? 'Buzz' : n.to_s) }
+        expect($stdout.string).to eq answer.join(' ')
       end
     end
 
