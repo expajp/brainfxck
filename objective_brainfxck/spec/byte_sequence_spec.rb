9ad295a29631d9c311a750dacec21311dfb9d921
Shu OGAWARA <expajp@gmail.com>
2019-06-16 13:30:35 +0900

    implement first test for byte_sequence

--- objective_brainfxck/spec/byte_sequence_spec.rb
+++ objective_brainfxck/spec/byte_sequence_spec.rb
@@ -1,4 +1,14 @@
 require './byte_sequence.rb'
 
 RSpec.describe ByteSequence do
+  class ByteSequence
+    attr_accessor :p
+  end
+  
+  describe 'unit tests' do
+    it 'right' do
+      byte_sequence = ByteSequence.new
+      expect{ byte_sequence.right }.to change{ byte_sequence.p }.by(1)
+    end
+  end
 end
